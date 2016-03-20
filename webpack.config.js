e540d9e5fcbff07a29ae6a64f589be5e5e99e195
Norbert Sienkiewicz <norbert.sienkiewicz@gmail.com>
2016-03-20 15:46:39 +0100

    initial template

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-    entry: './src',
+    entry: {
+      app: './src/index.ts',
+      vendor: './src/vendor.ts',
+    },
     output: {
-      path: 'dist',
-      filename: 'bundle.js'
+      path: './dist',
+      filename: 'bundle.js',
+      publicPath: "http://localhost:7070/"
     },
 
     plugins: [
@@ -18,12 +22,33 @@ module.exports = {
     ],
 
     resolve: {
-      extensions: ['', '.ts', '.js']
+      extensions: ['', '.ts', '.js', '.css']
     },
 
     module: {
       loaders: [
-        { test: /\.ts$/, loader: 'ts-loader' },
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader'
+        }, {
+          test: /\.css$/,
+          loader: 'style!css?sourceMap'
+        }, {
+          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=application/font-woff"
+        }, {
+          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=application/font-woff"
+        }, {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=application/octet-stream"
+        }, {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "file"
+        }, {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=image/svg+xml"
+        }
       ],
       noParse: [
         path.join(__dirname, 'node_modules', 'angular2', 'bundles')
